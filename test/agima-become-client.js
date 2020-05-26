56838201b551136fead69279aec003276a61c196
Pavel Nazarenko <p.nazarenko@dunice.net>
2020-05-26 17:28:48 +0300

    refactored

--- test/agima-become-client.js
+++ test/agima-become-client.js
@@ -2,23 +2,22 @@ const formSelector = 'form.new-customers__content-form';
 
 module.exports = {
   'Step 1: navigate to https://www.agima.ru/': function (browser) {
-    browser
+    return browser
       .url('https://www.agima.ru/')
       .click('body a.header__client')
-      .pause(500)
   },
   'Step 2: fill form' : function (browser) {
-    browser
+    return browser
+      .pause(500)
       .setValue(`${formSelector} input#name`, 'Test')
       .setValue(`${formSelector} input#email`, 'Test@test.ru')
       .setValue(`${formSelector} textarea#message`, 'test message')
       .click(`${formSelector} label[for="confirm-agreement"]`)
-      .pause(500)
   },
   'Step 3: send form' : function (browser) {
-    browser
+    return browser
+      .pause(100)
       .click(`${formSelector} a.js-submit`)
-      .pause(500)
       .assert.containsText('div.new-customers__popup', 'Спасибо')
       .end();
   }
